7936c7993158f8abd4960aa47dbeb51c6733c5b3
Jairo Arael Salazar Marin <lx16544@innovaccion.mx>
2022-11-11 20:16:40 -0600

    creando hola mundo node y servidor en localhost

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log ('hola mundo node');
+console.log ('hola mundo uwu ;D node');
+
+const http = require('http');
+
+const host = '0.0.0.0';
+
+const port = '3000';
+
+const server = http.createServer((req, res)=>{
+    res.statusCode =200;
+    res.setHeader =('Content-Type', 'text/plain');
+    res.end("Hola mundo UWU OKAS ");
+
+});
+
+server.listen(port, host, () => {
+    console.log("server encendido en " + host + "/" + port );
+});
